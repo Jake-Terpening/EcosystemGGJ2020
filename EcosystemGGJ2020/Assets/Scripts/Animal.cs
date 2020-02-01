af5e4a1866808ed86890b1e36d85abd8301a48ae
Jake <jaketerpening@gmail.com>
2020-02-01 10:35:12 -0800

    Revert "I think I unfixed it idk how to git"
    This reverts commit 48466e7f525dbb519bc7b0bd8658686fb652d0f1.

---
+++ EcosystemGGJ2020/Assets/Scripts/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : Consumable
+{
+    public string name;
+
+    public int foodPerCycle;
+    public int combatPower;
+    public float movePerCycle;
+
+    public int foodConsumed;
+
+    public bool herbivoir;
+    public bool carnivoir;
+
+    private Rigidbody2D rb2d;
+
+    public Vector2 debugMove;
+
+    void RunCycle()
+    {
+        
+    }
+
+
+    private void DebugMove()//force animal to go in a certain direction for debugging purposes
+    {
+        rb2d.velocity = debugMove;
+    }
+
+    private void Awake()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        DebugMove();
+    }
+}
